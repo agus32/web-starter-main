599092abbc90aac99a9b17736e7ffb99ade5d6b3
agus32 <agusluzzini@gmail.com>
2023-03-30 18:39:20 -0300

    resuelto hasta el ejecicio 4 :)

--- README.md
+++ README.md
@@ -10,11 +10,9 @@
 ## Ejercicios a realizar
 En base al proyecto inicial, realizar las siguientes modificaciones:
 
-3. En la sección Contacto: 
- - Agregar un campo para que el usuario pueda ingresar un teléfono de contacto. Si el mensaje se envía correctamente, mostrar el teléfono junto con el resto de los datos.
 
-4. En la sección donde obtenemos un usuario desde una api externa:
- - En el caso de darse un error (por ej por desconexión de internet), manejar el error y mostrar un mensaje al usuario: "No se ha podido recuperar el usuario. Por favor inténtelo más tarde."
+
+
 
 5. Estilos CSS:
  - Escribir una versión alternativa del archivo styles.css pero aplicando sass. Crear un archivo llamado styles.scss.
--- script.js
+++ script.js
@@ -6,17 +6,21 @@ window.addEventListener('load', () => {
         event.preventDefault();
         const name = document.querySelector('#name').value;
         const email = document.querySelector('#email').value;
+        const tel = document.querySelector('#tel').value;
         const message = document.querySelector('#message').value;
 
-        if (name !== '' && email !== '' && message !== '') {
+        if (name !== '' && email !== '' && message !== ''&& tel !== '') {
             // ok
             document.querySelector('#user-name').innerHTML = name;
             document.querySelector('#user-email').innerHTML = email;
+            document.querySelector('#user-tel').innerHTML = tel;
             document.querySelector('#user-message').innerHTML = message;
             document.querySelector('#success').classList.add('show-success');
+            document.querySelector('#error').classList.remove('show-error');
         } else {
             // error
             document.querySelector('#error').classList.add('show-error');
+            document.querySelector('#success').classList.remove('show-success');
         }
     });
 
@@ -32,6 +36,15 @@ function getUser() {
             const userData = response.results[0].name;
             document.querySelector('#user-name').innerHTML = `${userData.title}. ${userData.first} ${userData.last}`;
             document.getElementById("api-img").setAttribute("src", response.results[0].picture.large); 
+            document.getElementById("fetch-error").classList.remove('show-error');
+            document.getElementById("fetch-error").innerHTML = "";
         })
-        .catch((error) => console.log('aja', error));
+        .catch((error) => {
+            console.log('aja', error);
+            document.getElementById("fetch-error").innerHTML = `No se ha podido recuperar el usuario. Por favor inténtelo más tarde. (${error}).`;
+
+    
+    
+    
+    });
 }
